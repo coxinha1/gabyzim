8a7a15dd97592b375e44a4d706f413fb020353ad
coxinha1 <125052661+coxinha1@users.noreply.github.com>
2024-03-13 11:34:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
+
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+
+contadores[0].textContent = calculatenpo(tenpoObjetivol);
+
+function calculatenpo(tenpoObjetivol){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1
+    let segundo = tempoFinal/1000;
+    return segundo;
+    }
+
